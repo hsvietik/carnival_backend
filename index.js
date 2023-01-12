6f2ab2574b02d26ea235f0d78d3b80ea164ee460
Hanna Svietik <hsvietik@gmail.com>
2023-01-12 12:18:52 +0000

    changed for render

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://carnival-app.onrender.com"],
+    origin: ["http://localhost:3000", "https://carnival-booking.onrender.com"],
   })
 );
 
